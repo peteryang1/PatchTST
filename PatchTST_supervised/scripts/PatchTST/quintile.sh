7ea1438a36166b35b3ded10281377bcdec7711da
Xu Yang <xuyang1@microsoft.com>
2023-05-19 06:17:12 +0000

    update script

--- PatchTST_supervised/scripts/PatchTST/quintile.sh
+++ PatchTST_supervised/scripts/PatchTST/quintile.sh
@@ -13,7 +13,7 @@ data_path_name=quintile_stocka.csv
 model_id_name=quintile_stocka
 data_name=custom
 
-random_seed=42
+random_seed=2021
 for pred_len in 96 192
 do
     python -u run_longExp.py \
@@ -21,7 +21,7 @@ do
       --is_training 1 \
       --root_path $root_path_name \
       --data_path $data_path_name \
-      --model_id $model_id_name_$seq_len'_'$pred_len \
+      --model_id quintile_stocka_$seq_len'_'$pred_len \
       --model $model_name \
       --data $data_name \
       --features M \
